08181c2ae860cbc37b473890ff8b5d512c9bd700
Alvian Rahman Hanif <alvian524@gmail.com>
2017-12-28 14:04:46 +0700

    setup default data

--- src/store/app/state.js
+++ src/store/app/state.js
@@ -2,7 +2,23 @@ export default {
   data: {
     name: 'data',
     value: {
-      title: '...'
+      title: '...',
+      motto: '...',
+      description: '...',
+      list: [],
+      links: {
+        facebook: '...',
+        instagram: '...'
+      },
+      contact: {
+        email: '...',
+        phone: '...'
+      },
+      address: {
+        text: '...',
+        longitude: '...',
+        latitude: '...'
+      }
     }
   }
 }
