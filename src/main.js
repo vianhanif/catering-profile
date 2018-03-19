3b1fe2ad90ddf354b6615319d8167725ef7a1208
Alvian Rahman Hanif <alvian524@gmail.com>
2018-03-19 17:19:59 +0700

    add analytics

--- src/main.js
+++ src/main.js
@@ -1,13 +1,28 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.config.base with an alias.
 import Vue from 'vue'
+import VueAnalytics from 'vue-analytics'
 import App from './App'
 import router from './router'
 import store from './store'
 import 'assets/css/vendor.css'
 
 Vue.config.productionTip = false
-
+Vue.use(VueAnalytics, {
+  id: 'UA-84069724-3',
+  router,
+  autoTracking: {
+    page: true,
+    transformQueryString: false,
+    pageviewTemplate (route) {
+      return {
+        page: route.path,
+        title: route.name,
+        location: window.location.href
+      }
+    }
+  }
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
